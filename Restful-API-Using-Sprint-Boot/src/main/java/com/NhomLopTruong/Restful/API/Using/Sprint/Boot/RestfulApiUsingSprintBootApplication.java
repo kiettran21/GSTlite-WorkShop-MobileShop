dce8ac4b719ef190a87f6d989d1f6a353932ed70
Kiet Tran <17110165@student.hcmute.edu.vn>
2020-06-29 19:57:17 +0700

    Create API Spring Boot Project

---
+++ Restful-API-Using-Sprint-Boot/src/main/java/com/NhomLopTruong/Restful/API/Using/Sprint/Boot/RestfulApiUsingSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.NhomLopTruong.Restful.API.Using.Sprint.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiUsingSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiUsingSprintBootApplication.class, args);
+	}
+
+}
---
+++ Restful-API-Using-Sprint-Boot/src/test/java/com/NhomLopTruong/Restful/API/Using/Sprint/Boot/RestfulApiUsingSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NhomLopTruong.Restful.API.Using.Sprint.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiUsingSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
