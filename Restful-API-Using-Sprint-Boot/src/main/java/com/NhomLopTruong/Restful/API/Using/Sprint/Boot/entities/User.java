212e2b9742aa39871aee530a535e37fbcb2f4cdd
nguyendat1234 <47876667+nguyendat1234@users.noreply.github.com>
2020-07-05 04:47:51 +0700

    Thư viện ResFul

--- Restful-API-Using-Sprint-Boot/src/main/java/com/NhomLopTruong/Restful/API/Using/Sprint/Boot/entities/User.java
+++ Restful-API-Using-Sprint-Boot/src/main/java/com/NhomLopTruong/Restful/API/Using/Sprint/Boot/entities/User.java
@@ -18,12 +18,36 @@ import lombok.Data;
 @Data
 public class User implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    private String username;
+	private String username;
 
-    private String password;
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
